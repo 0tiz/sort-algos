77214551eea60015a00f598a50155b03ea04daf2
0tiz <0tiz@users.noreply.github.com>
2018-05-15 17:07:13 +0200

    class

--- InsertionSort/InsertionSort/Program.cs
+++ InsertionSort/InsertionSort/Program.cs
@@ -11,7 +11,7 @@ namespace InsertionSort
         static void Main(string[] args)
         {
             int[] unsortArray = new int[5] { 12, 11, 15, 3, 7 };
-            CocktailSort(unsortArray);
+            ShakerSort.Sort(unsortArray);
             Console.WriteLine(String.Join(", ", unsortArray));
         }
 
@@ -41,45 +41,6 @@ namespace InsertionSort
                 }
 
 
-            }
-
-        }
-
-        private static void CocktailSort(int[] array)
-        {
-            bool didSwap = true;
-            int lowerBound = 1;
-            int upperBound = array.Length;
-            while (didSwap == true)
-            {
-
-                didSwap = false;
-
-                for (int i = lowerBound; i < upperBound; i++)
-                {
-                    if (array[i - 1] > array[i])
-                    {
-                        int cache = 0;
-                        cache = array[i];
-                        array[i] = array[i - 1];
-                        array[i - 1] = cache;
-                        didSwap = true;
-                    }
-                }
-                upperBound = upperBound - 1;
-
-                for (int i = upperBound - 1; i > lowerBound - 1; i--)
-                {
-                    if (array[i - 1] > array[i])
-                    {
-                        int cache = 0;
-                        cache = array[i];
-                        array[i] = array[i - 1];
-                        array[i - 1] = cache;
-                        didSwap = true;
-                    }
-                }
-                lowerBound = lowerBound + 1;
             }
 
         }
---
+++ InsertionSort/InsertionSort/ShakerSort.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    static class ShakerSort
+    {
+        public static void Sort(int[] array)
+        {
+            bool didSwap = true;
+            int lowerBound = 1;
+            int upperBound = array.Length;
+            while (didSwap == true)
+            {
+
+                didSwap = false;
+
+                for (int i = lowerBound; i < upperBound; i++)
+                {
+                    if (array[i - 1] > array[i])
+                    {
+                        int cache = 0;
+                        cache = array[i];
+                        array[i] = array[i - 1];
+                        array[i - 1] = cache;
+                        didSwap = true;
+                    }
+                }
+                upperBound = upperBound - 1;
+
+                for (int i = upperBound - 1; i > lowerBound - 1; i--)
+                {
+                    if (array[i - 1] > array[i])
+                    {
+                        int cache = 0;
+                        cache = array[i];
+                        array[i] = array[i - 1];
+                        array[i - 1] = cache;
+                        didSwap = true;
+                    }
+                }
+                lowerBound = lowerBound + 1;
+            }
+
+        }
+
+    }
+}
